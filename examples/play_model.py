c4b46629e2285549e221aaf37a5c2ff4dcb12667
loncsemi <michaelloncsek@Michaels-MacBook-Air.local>
2024-06-10 09:49:30 +0200

    made 3D plot optional

--- examples/play_model.py
+++ examples/play_model.py
@@ -5,6 +5,7 @@ from train_utils import process_observation, save_observations
 import datetime
 from stable_baselines3.common.vec_env import DummyVecEnv
 import os
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def play_trained_model(model_path: str, config_path: str, gui: bool = False,episodes_path: str = "episodes/"):
@@ -16,9 +17,10 @@ def play_trained_model(model_path: str, config_path: str, gui: bool = False,epis
     # Set the model's environment
     model.set_env(env)
     # Play the model in the environment
-    episodes = 100
+    episodes = 5
     for i in range(episodes):
         obs_list = []
+        action_list = []
         x = env.reset()
         process_observation(x, False)
         done = False
@@ -26,21 +28,45 @@ def play_trained_model(model_path: str, config_path: str, gui: bool = False,epis
         episode_length = 0
         while not done:
             action, *_ = model.predict(x)
+            action_list.append(action)
+            #print(action)
             x, r, done, info = env.step(action)
             ret += r
             episode_length += 1
             obs_list.append(process_observation(x, False))
         # Save the observations
-        print(f"Episode {i}: Episode Length: {episode_length}")
+        print(f"Episode {i}: Return: {ret}, Episode Length: {episode_length}")
         save_path = episodes_path + "episodes"
         save_path = Path(save_path)
         save_path.mkdir(parents=True, exist_ok=True)
-        save_observations(obs_list, save_path, i)    
-    
+        save_observations(obs_list, save_path, i)
+
+        # plot actions in 3D
+        import matplotlib.pyplot as plt
+
+        # Extract x, y, z coordinates from the action_list
+        x_coords = [action[0][0] for action in action_list]
+        y_coords = [action[0][1] for action in action_list]
+        z_coords = [action[0][2] for action in action_list]
+
+        # Create a 3D plot
+        if False:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot(x_coords, y_coords, z_coords)
+
+            # Set labels and title
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            ax.set_title('Actions in 3D')
+            # Show the plot
+            plt.show()
+
     return ret, episode_length
 
 if __name__ == '__main__':
-    model_path = "trained_models/2024-06-03_10-00-58/model_10000_steps.zip"
+    model_path = "trained_models/2024-06-06_21-44-07/best_model.zip"
     episodes_path = os.path.dirname(model_path) + "/"
     config_path = "config/getting_started.yaml"
     ret, episode_length = play_trained_model(model_path, config_path, 100, episodes_path)
